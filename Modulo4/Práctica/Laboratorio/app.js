06ed2c6c765382127f3758fa8b3dd5ea6aa673c8
XkryGit <99211061+XkryGit@users.noreply.github.com>
2022-02-23 06:41:58 +0100

    Add files via upload

---
+++ Modulo4/Práctica/Laboratorio/app.js
@@ -0,0 +1,54 @@
+//Calculator
+
+//Inputs
+var number1 = () => parseInt(document.getElementById("inputnumber1").value);
+var number2 = () => parseInt(document.getElementById("inputnumber2").value);
+
+//Operations
+var sumvar = () => number1() + number2();
+var subtractionvar = () => number1() - number2();
+var multiplicationvar = () => number1() * number2();
+var divisionvar = () => number1() / number2();
+
+//Resolves
+var resultsum = () => document.getElementById("seeresult").innerText = sumvar();
+var resultsubtraction = () => document.getElementById("seeresult").innerText = subtractionvar();
+var resultmultiplication = () => document.getElementById("seeresult").innerText = multiplicationvar();
+var resultdivision = () => document.getElementById("seeresult").innerText = divisionvar();
+
+//Events
+document.getElementById("sum").addEventListener("click", ifresultsum);
+document.getElementById("subtraction").addEventListener("click", ifresultsubtraction);
+document.getElementById("multiplication").addEventListener("click", ifresultmultiplication);
+document.getElementById("division").addEventListener("click", ifresultdivision);
+
+
+//Conditions
+function ifresultsum () {
+    if (isNaN(number1()) || isNaN(number2())) {
+        document.getElementById("seeresult").innerText = "Error";
+    } else {
+        resultsum();
+    }
+}
+function ifresultsubtraction () {
+    if (isNaN(number1()) || isNaN(number2())) {
+        document.getElementById("seeresult").innerText = "Error";
+    } else {
+        resultsubtraction();
+    }
+}
+function ifresultmultiplication () {
+    if (isNaN(number1()) || isNaN(number2())) {
+        document.getElementById("seeresult").innerText = "Error";
+    } else {
+        resultmultiplication();
+    }
+}
+function ifresultdivision () {
+    if (isNaN(number1()) || isNaN(number2())) {
+        document.getElementById("seeresult").innerText = "Error";
+    } else {
+        resultdivision();
+    }
+}
