600df1b841367f152e13b51bf745672320414777
XkryGit <99211061+XkryGit@users.noreply.github.com>
2022-09-15 10:10:00 +0200

    Delete Modulo 12A/src directory

--- Modulo 12A/src/pages/account-list/account-list.api.js
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios';
-const url = `${process.env.BASE_API_URL}/account-list`;
-
-export const getAccountList = () => Axios.get(url).then(({ data }) =>
-data);
-
-
-export const getAccountListMov = (id) => Axios.get(`${url}/${id}`).then(({ data }) => data);
